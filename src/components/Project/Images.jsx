4f4096336f9ff71473167ebee1d8d63bcd520ee5
estherlardze <estherlardze8@gmail.com>
2023-11-20 01:34:42 +0000

    prevent image download

--- src/components/Project/Images.jsx
+++ src/components/Project/Images.jsx
@@ -1,37 +1,44 @@
-import { useState } from 'react'
-import { data } from './index'
-import Popup from './Popup'
-import Loader from '../Loader'
+import { useState } from 'react';
+import { data } from './index';
+import Popup from './Popup';
+import Loader from '../Loader';
 
 const Images = () => {
-const [showImage, setShowImage] = useState(false)
-const [selectedImageData, setSelectedImageData] = useState(null);
+  const [showImage, setShowImage] = useState(false);
+  const [selectedImageData, setSelectedImageData] = useState(null);
 
-
-const onImageClick = (item) => {
-    setShowImage(true)
+  const onImageClick = (item) => {
+    setShowImage(true);
     setSelectedImageData(item);
-}
+  };
 
- if (!data.length) return <Loader/>
+  if (!data.length) return <Loader />;
+
+  const preventRightClick = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <section className='my-[50px] w-[90%] mx-[5%] sm:w-[94%] sm:mx-[3%] mb-[100px]'>
-        <div className='flex gap-3 flex-wrap justify-center items-center lg:justify-start'>
-            {data.map((item) => (
-               <article key={item.id} onClick={() => onImageClick(item)} data-aos="zoom-in">
-                 <img 
-                   src={item.image} 
-                   alt={`item ${item.id}`}
-                    className='w-full sm:w-[120px] cursor-pointer'
-                   />              
-               </article>      
-            ))}
-        </div>
-     
-    <Popup showImage={showImage} setShowImage={setShowImage} selectedImageData={selectedImageData} data={data}/>
-    </section>
-  )
-}
+      <div
+        className='flex gap-3 flex-wrap justify-center items-center lg:justify-start'
+        onContextMenu={preventRightClick} 
+      >
+        {data.map((item) => (
+          <article key={item.id} onClick={() => onImageClick(item)} data-aos="zoom-in">
+            <img
+              src={item.image}
+              alt={`item ${item.id}`}
+              className='w-full sm:w-[120px] cursor-pointer'
+              onContextMenu={preventRightClick} 
+            />
+          </article>
+        ))}
+      </div>
 
-export default Images
+      <Popup showImage={showImage} setShowImage={setShowImage} selectedImageData={selectedImageData} data={data} />
+    </section>
+  );
+};
+
+export default Images;
--- src/components/Project/Popup.jsx
+++ src/components/Project/Popup.jsx
@@ -1,39 +1,46 @@
-import {AiOutlineClose, AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai'
+import { AiOutlineClose, AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import { useState, useEffect } from 'react';
 
-
-const Popup = ({showImage, setShowImage, selectedImageData}) => {
+const Popup = ({ showImage, setShowImage, selectedImageData }) => {
 
   const handleImage = () => {
     setShowImage(false);
-  }
+  };
+
+  const preventRightClick = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
-    {showImage && selectedImageData && (
+      {showImage && selectedImageData && (
         <section className='bg-black/95 w-full h-screen fixed left-0 top-0 z-50 flex flex-col justify-center items-center mb-10'>
-          <div className='flex justify-center items-start w-full mx-10 sm:mx-10 gap-10 mb-10'>
-           <img 
-             src={selectedImageData.image} 
-             alt={`item ${selectedImageData.id}`} 
-             className='w-[80%] h-[50vh] sm:h-[70] md:h-[80vh]'
-             />
+          <div
+            className='flex justify-center items-start w-full mx-10 sm:mx-10 gap-10 mb-10'
+            onContextMenu={preventRightClick} 
+          >
+            <img
+              src={selectedImageData.image}
+              alt={`item ${selectedImageData.id}`}
+              className='w-[80%] h-[50vh] sm:h-[70] md:h-[80vh]'
+              onContextMenu={preventRightClick}
+            />
 
-            <AiOutlineClose  
-              size={25} 
+            <AiOutlineClose
+              size={25}
               className='cursor-pointer text-white font-bold'
               onClick={handleImage}
-           />
-           </div>
-     
-         {/* <div className='flex items-center justify-center gap-10'>
-          <AiOutlineArrowLeft size={25} className='cursor-pointer text-white font-bold'/>
-          <AiOutlineArrowRight size={25} className='cursor-pointer text-white font-bold'/>
-        </div> */}
-      </section>
-    )}</>
-    
-  )
-}
+            />
+          </div>
 
-export default Popup
+          {/* <div className='flex items-center justify-center gap-10'>
+            <AiOutlineArrowLeft size={25} className='cursor-pointer text-white font-bold' />
+            <AiOutlineArrowRight size={25} className='cursor-pointer text-white font-bold' />
+          </div> */}
+        </section>
+      )}
+    </>
+  );
+};
+
+export default Popup;
